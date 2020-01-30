23457440750d8da2ce85b7ef0f12fbd7d4787f81
Malte Schwarzkopf <malte@cs.brown.edu>
2020-01-30 15:25:32 -0500

    Fixes

--- l03/mexplore-string.c
+++ l03/mexplore-string.c
@@ -5,13 +5,13 @@
 
 //                | static, const global string; in static segment
 //                | of memory (= static lifetime).
-//                | (can't be modified, because it's in the constant
-//                | section of the static memor segment)
+//                | (can't be modified, because it's in the read-only
+//                | section of the static memory segment)
 //                v
 char* global_st = "Hello CS131!";
 
 void f() {
-    hexdump(global_st, 13);
+    hexdump(global_st, 13);  // 12 characters plus NUL terminator
 
     //               | static, const string (read-only).
     //               | Literal strings are in the read-only section of
@@ -22,12 +22,13 @@ void f() {
     //               | segment.
     //               v
     char* local_st = "We <3 systems";
-    hexdump(local_st, 15);
+    hexdump(local_st, 14);  // 13 characters plus NUL terminator
 
     // This segfaults (as it did in the lecture) because it tries to
     // modify a ready-only literal string.
     //for (int i = 0; i < 13; i++) {
     //  if (*(local_st + i) == '<') {
+    //    *(local_st + i) = 'E';
     //    *(local_st + i) = '>';
     //  }
     //}
@@ -43,7 +44,7 @@ void f() {
       }
     }
 
-    hexdump(allocated_st, 15);
+    hexdump(allocated_st, 14);  // 13 characters plus NUL terminator
 }
 
 int main() {
