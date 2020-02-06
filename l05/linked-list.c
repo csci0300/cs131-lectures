ef14f5c142870a64047794ae17f64ca041344433
Malte Schwarzkopf <malte@cs.brown.edu>
2020-02-06 17:03:39 -0500

    LEC 5 post-lecture updates

--- l05/linked-list.c
+++ l05/linked-list.c
@@ -2,20 +2,48 @@
 #include <stdlib.h>
 #include "hexdump.h"
 
+typedef struct list_node {
+  int value;
+  struct list_node* next;
+} list_node_t;
+
 typedef struct {
+  list_node_t* head;
 } list_t;
 
-void append(list_t* l, int v) {
+void append(list_t* list, int value) {
+  list_node_t* cur = list->head;
+
+  if (cur != NULL) {
+    while (cur->next != NULL) {
+      cur = cur->next;
+    }
+  }
+
+  list_node_t* new_node = (list_node_t*)malloc(sizeof(list_node_t));
+  new_node->next = NULL;
+  new_node->value = value;
+
+  if (cur != NULL) {
+    cur->next = new_node;
+  } else {
+    list->head = new_node;
+  }
 }
 
 int main() {
-  list_t* l = malloc(sizeof(list_t));
+  list_t* l = (list_t*)malloc(sizeof(list_t));
+  l->head = NULL;
 
   for (int i = 0; i < 100; i++) {
     append(l, i);
   }
 
   hexdump(l, sizeof(list_t));
+  hexdump(l->head, sizeof(list_node_t));
+  hexdump(l->head->next->next->next->next, sizeof(list_node_t));
+
+  printf("sizeof(list_node_t): %lu\n", sizeof(list_node_t));
 
   free(l);
 }
--- l05/mexplore-struct.c
+++ l05/mexplore-struct.c
@@ -5,29 +5,33 @@
 #include <stddef.h>
 #include "hexdump.h"
 
-struct x_t {
+typedef struct x {
   int i1;
   int i2;
   int i3;
   char c1;
   char c2;
   char c3;
-};
+} x_t;
 
 void f() {
-  struct x_t x;
-  x.i1 = 1;
+  x_t* x = (x_t*)malloc(sizeof(x_t));
+  x->i1 = 1;
+  x->i2 = 2;
+  x->c3 = 'X';
+  printf("i2: %d\n", x->i2);
 
   printf("whole struct:\n");
-  hexdump(&x, sizeof(x));
+  hexdump(x, sizeof(x_t));
+
 
   printf("\nmembers:\n");
-  hexdump(&x.i1, sizeof(x.i1));
-  hexdump(&x.i2, sizeof(x.i2));
-  hexdump(&x.i3, sizeof(x.i3));
-  hexdump(&x.c1, sizeof(x.c1));
-  hexdump(&x.c2, sizeof(x.c2));
-  hexdump(&x.c3, sizeof(x.c3));
+  hexdump(&x->i1, sizeof(x->i1));
+  hexdump(&x->i2, sizeof(x->i2));
+  hexdump(&x->i3, sizeof(x->i3));
+  hexdump(&x->c1, sizeof(x->c1));
+  hexdump(&x->c2, sizeof(x->c2));
+  hexdump(&x->c3, sizeof(x->c3));
 }
 
 int main() {
