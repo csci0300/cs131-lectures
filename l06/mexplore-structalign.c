e54a46858c5ba65d6251c60e963e15f08ff6e01b
Malte Schwarzkopf <malte@cs.brown.edu>
2020-02-11 15:38:45 -0500

    Update Lecture 6 code

--- l06/mexplore-structalign.c
+++ l06/mexplore-structalign.c
@@ -6,6 +6,7 @@
 #include <stdalign.h>
 #include "hexdump.h"
 
+// try using a typedef with this to make ints_and_chars_t!
 struct ints_and_chars {
   int i1;
   char c1;
@@ -16,10 +17,14 @@ struct ints_and_chars {
 };
 
 void f() {
-  struct ints_and_chars x = {.i1 = 1, .c1 = 'A', .i2 = 2, .c2 = 'B', .i3 = 3, .c3 = 'C'};
+  // try changing this to a heap-allocated struct via malloc!
+  struct ints_and_chars* x = {.i1 = 1, .c1 = 'A', .i2 = 2, .c2 = 'B', .i3 = 3, .c3 = 'C'};
 
-  printf("sizeof(struct ints_and_chars): %lu\n", sizeof(struct ints_and_chars));
-  printf("alignof(struct ints_and_chars): %lu\n", alignof(struct ints_and_chars));
+  printf("sizeof: %lu\n", sizeof(struct ints_and_chars));
+  printf("alignof: %lu\n", alignof(struct ints_and_chars));
+
+  printf("\nwhole struct:\n");
+  hexdump(&x, sizeof(struct ints_and_chars));
 
   printf("\nmembers:\n");
   hexdump(&x.i1, sizeof(x.i1));
